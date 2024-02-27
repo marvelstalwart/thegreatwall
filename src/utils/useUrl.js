7008c161e0700a04195ca56d54e4866345de739c
Marvelstalwart <Marvelstalwart@gmail.com>
2024-02-27 21:53:48 +0100

    [Add] Update server url

--- src/utils/useUrl.js
+++ src/utils/useUrl.js
@@ -1,7 +1,8 @@
 
 export default function getHost() {
     const __isProd__ = process.env.NODE_ENV === 'production'
-    const host = __isProd__ ? 'https://giftcard-server.onrender.com' : 'http://localhost:5000'
+    const host = __isProd__ ? 'https://mygiftcardserver.onrender.com' : 'http://localhost:5000'
   return host
 }
+
 
